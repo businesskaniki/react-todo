82454f3e7517be9304755b28f701710292209754
businesskaniki <businesskanikii@gmail.com>
2022-08-19 00:27:48 +0300

    added functional components

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import  ReactDOM  from "react-dom";
+import "./app.css"
+//components import
+import TodoContainer from "./components/TodoContainer";
+
+ReactDOM.render(
+
+<React.StrictMode>
+    <TodoContainer />
+</React.StrictMode> ,
+
+document.getElementById("root"))
